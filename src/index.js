d83e630a039d208c52a984b259ac1d8137bb13e1
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-03-27 23:05:50 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import { store } from 'redux'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
 
 
