33f2c4f5e75d4414fcaf41e293c9468ba6678bff
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-05-31 15:46:27 +0530

    fix - sort filter

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -5,14 +5,25 @@ import useFetch from "../../hooks/useFetch";
 
 const List = ({ subCats, maxPrice, sort, catId }) => {
 
+  let fetchUrlString = `/products?populate=*&[filters][categories][id]=${catId}&[filters][price][$lte]=${maxPrice}`
+
   // console.log(subCats);
   const subCatString = subCats.map(
     (item, idx) => `[filters][sub_categories][id][$in][${idx}]=${item}`
   ).join('&');
+
+  if(subCats.length > 0){
+    fetchUrlString = fetchUrlString + '&' + subCatString
+  }
+
+  if(sort){
+    fetchUrlString = fetchUrlString + `&sort[0]=price:${sort}`
+  }
+
+  console.log(fetchUrlString);
   
   
-  const { data, loading, error } = useFetch( `/products?populate=*&[filters][categories][id]=${catId}&${subCatString}`
-  );
+  const { data, loading, error } = useFetch( fetchUrlString);
 
   return (
     <div className="list">
